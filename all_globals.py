e9d2341354e52d14945a1872704e2ea13899ba11
titanicsummerghost <82958857+muhidabid@users.noreply.github.com>
2021-11-15 20:54:08 +0500

    Add multiple watermarks onto multiple pictures!

---
+++ all_globals.py
@@ -0,0 +1,24 @@
+def initialize():  
+    global max_logo_size      # this is the percent logo will be of the shorter side of the image
+    max_logo_size = 10
+    
+    global values_master
+    values_master = {}
+    
+    global logo_choice
+    logo_choice = {}
+
+    global pictures_directory
+    pictures_directory = ''
+    
+    global logos_directory
+    logos_directory = ''
+    
+    global withlogo_directory
+    withlogo_directory = ''
+
+    global frames
+    frames = []
+    
+    global widgets
+    widgets = []
